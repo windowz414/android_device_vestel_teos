70a90c999cd515630db33b2705b6fce6d4ad8df1
Beru Shinsetsu <windowz414@1337.lgbt>
2022-02-28 10:43:33 +0000

    teos/charger: Initial commit
    * The whole logic behind this is to get rid of CM charger res imgs,
      which are quite large on our device's screen anyway, and replace
      the other dependencies provided by CM with AOSP/CAF ones. Our
      custom charger res product package, `teos_charger_res_images`,
      holds the purpose of overriding AOSP-provided `charger_res_images`,
      even though we added it as a package first.
    
    * Charger images were taken from stock ROM dump.
    Limk: https://github.com/windowz414/vestel_teos_dump/tree/Teos-user-7.1.2-VTE1190-131806-release-keys/boot/ramdisk/res/images/charger
    
    * Makefile for `teos_charger_res_images` was made under light of
      `cm_charger_res_inages` package definitions.
    Link: https://github.com/LineageOS/android_vendor_cm/blob/cm-14.1/charger/Android.mk#L45-L70
    
    * Other possibly required packages were found under light of
      product package definitions for CM Charger and packages introduced
      in `system/core/healthd`.
    Link: https://github.com/LineageOS/android_vendor_cm/blob/95af010925cb7725ebe687b35353f2234af0af8f/config/common.mk#L163-L170
    Link: https://github.com/LineageOS/android_system_core/blob/b149ecb4a97fb9088f6912b98922ed946d47bef7/healthd/Android.mk#L21-L27
    Link: https://github.com/LineageOS/android_system_core/blob/b149ecb4a97fb9088f6912b98922ed946d47bef7/healthd/Android.mk#L87-L149
    
    Signed-off-by: Beru Shinsetsu <windowz414@1337.lgbt>

--- device.mk
+++ device.mk
@@ -126,6 +126,15 @@ PRODUCT_PACKAGES += \
     libmm-qcamera \
     Snap
 
+# Charger Resource Images (Teos)
+WITH_CM_CHARGER := false
+PRODUCT_PACKAGES += \
+    charger \
+    charger_res_images \
+    libhealthd.qcom \
+    healthd \
+    teos_charger_res_images
+
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8937 \
