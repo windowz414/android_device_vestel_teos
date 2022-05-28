3aa07392034fd6b74c8f9baacb12880a3738e808
Beru Hinode <windowz414@1337.lgbt>
2022-05-28 13:33:10 +0300

    teos: Huge prop changes again
    * BC: Add OEM partition support
    
    * {extraprops,system.prop}: Get much closer to stock props
    
    * makefile: Remove fingerprint and fully depend on thumbprint
    
    Signed-off-by: Beru Hinode <windowz414@1337.lgbt>

--- extraprops.mk
+++ extraprops.mk
@@ -1,7 +1,23 @@
+# Delegation for OEM customization
+PRODUCT_OEM_PROPERTIES += \
+    ro.product.device \
+    ro.product.name \
+    ro.product.manufacturer \
+    ro.product.model \
+    ro.product.brand \
+    ro.config.ringtone \
+    ro.config.notification_sound \
+    ro.config.alarm_alert \
+    ro.config.wallpaper \
+    ro.config.manual_path \
+    ro.product.locale \
+    ro.oem.* \
+    oem.* \
+
+
 # Extra properties needed outside of system.prop
 
-# Google Assistant
-PRODUCT_PROPERTY_OVERRIDES += ro.opa.eligible_device=true
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.mb_serialno=0
 
 #
 # ADDITIONAL BUILD PROPERTIES
@@ -11,10 +27,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.audio.fluence.audiorec=false \
     persist.audio.fluence.voicerec=false \
     ro.frp.pst=/dev/block/bootdevice/by-name/config \
+    persist.sys.version=1.19.0 \
     persist.radio.calls.on.ims=true \
     persist.radio.jbims=1 \
     persist.radio.VT_HYBRID_ENABLE=1 \
     persist.radio.user.add.eccList=911,144,110,112,121,141,151,132,153,154,155,156,157,158,159,168,177,183,171,122,136,131 \
     persist.nfc.smartcard.config=SIM1,eSE1 \
     persist.somc.dbg.ims_vilte_on=1 \
-    ro.mdtp.package_name2=com.qualcomm.qti.securemsm.mdtp.MdtpDemo
+    ro.mdtp.package_name2=com.qualcomm.qti.securemsm.mdtp.MdtpDemo \
+    ro.com.android.dataroaming=false \
+    ro.com.android.mobiledata=true \
+    ro.com.google.gmsversion=7.1_r201803 \
+    ro.setupwizard.mode=OPTIONAL
